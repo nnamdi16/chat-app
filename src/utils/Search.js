6c07b90ad98c4f175c1f24e4d21d687ffb1f93cb
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-13 16:51:53 +0100

    Create util function for Searching books

---
+++ src/utils/Search.js
@@ -0,0 +1,13 @@
+import * as BooksAPI from '../utils/BooksAPI';
+
+export const Search = query =>
+	BooksAPI.search(query, 20)
+		.then(res => res.map(book => book.id))
+		.then(res => [...new Set(res)])
+		.then(myArray => ({
+			searchResults: myArray
+		}))
+		.catch(e => {
+			console.log(e);
+			return [];
+		});
