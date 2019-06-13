78e427bc4addcb12bc36e5eee7b603e966197609
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-13 16:43:44 +0100

    Create utility function for updating the shelf and querying the database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { debounce } from 'lodash';
 import './App.css';
+import * as BooksAPI from './utils/BooksAPI';
+import Shelf from './components/Shelf';
+import SearchBooks from './components/SearchBooks';
+class BookApp extends Component {
+	state = {
+		query: '',
+		currentlyReading: [],
+		wantToRead: [],
+		read: [],
+		searchResults: []
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	componentDidMount() {
+		BooksAPI.update({ id: 'dummy' }, 'none')
+			.then(shelvesObject => this.updateShelf(shelvesObject))
+			.then(() => console.log('App: component did mount, book shelves were just updated.'))
+			.catch(e => {
+				console.log(e);
+				return [];
+			});
+	}
+
+	updateShelf = shelvesObject => {
+		this.setState(shelvesObject);
+		console.log('Books in the Shelf updated!');
+	};
+
+	updateQuery = query => {
+		console.log(query);
+		debounce(this.setState({ query: query }), 200);
+	};
+	render() {
+		return (
+			<div className="app">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				<div className="list-books">
+					<Route
+						exact
+						path="/search/"
+						render={() => (
+							<div>
+								<SearchBooks query={this.state.query} updateQuery={this.updateQuery} updateShelf={this.updateShelf} />
+								<div className="search-books-results">
+									<Shelf
+										ShelfName="Search Results"
+										updateShelf={this.updateShelf}
+										ThisShelf={this.state.searchResults}
+									/>
+								</div>
+							</div>
+						)}
+					/>
+
+					<Route
+						path="/search/:urlQuery"
+						render={({ match }) => (
+							<div>
+								<SearchBooks
+									urlQuery={match.params.urlQuery}
+									query={this.state.query}
+									updateQuery={this.updateQuery}
+									updateShelf={this.updateShelf}
+								/>
+								<div className="search-books-results">
+									<Shelf
+										ShelfName="Search Results"
+										updateShelf={this.updateShelf}
+										ThisShelf={this.state.searchResults}
+									/>
+								</div>
+							</div>
+						)}
+					/>
+
+					<Route
+						exact
+						path="/"
+						render={() => (
+							<div>
+								<div className="list-books-content">
+									<Shelf
+										ShelfName="Currently Reading"
+										updateShelf={this.updateShelf}
+										ThisShelf={this.state.currentlyReading}
+									/>
+									<Shelf ShelfName="Want To Read" updateShelf={this.updateShelf} ThisShelf={this.state.wantToRead} />
+									<Shelf ShelfName="Read" updateShelf={this.updateShelf} ThisShelf={this.state.read} />
+								</div>
+
+								<div className="open-search">
+									<Link to={`/search/${this.state.query}`}>Add a book</Link>
+								</div>
+							</div>
+						)}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default BookApp;
